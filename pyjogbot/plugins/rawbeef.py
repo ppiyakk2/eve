9922a2bdad145219084d08c13aaf3e1416033fd9
Junhyun Kim <kjunh816@gmail.com>
2016-08-23 07:54:10 +0000

    Modified unnecessary assignments, ambiguous variable names

--- pyjogbot/plugins/rawbeef.py
+++ pyjogbot/plugins/rawbeef.py
@@ -4,22 +4,19 @@ import datetime
 
 
 def calc_next_rawbeef():
-    next_rawbeef = datetime.date(2016, 9, 3)
+    next_date = datetime.date(2016, 9, 3)
     next_count = 46
     today = datetime.date.today()
+    while today >= next_date:
+        next_date += datetime.timedelta(weeks=20)
+        next_count += 10
 
-    while today >= next_rawbeef:
-        last_rawbeef = next_rawbeef
-        last_count = next_count
-        next_rawbeef = last_rawbeef + datetime.timedelta(weeks=20)
-        next_count = last_count + 10
-
-    return next_rawbeef, next_count
+    return next_date, next_count
 
 
 @respond_to('육회', re.IGNORECASE)
 def next_rawbeef(message):
-    next = calc_next_rawbeef()
+    ret = calc_next_rawbeef()
     message.send("다음 육회조그(%d회)는 별다른 일이 없다면 %s년 %s월 %s일 입니다(D-%d)."\
-		 % (next[1], next[0].year, next[0].month, next[0].day, (next[0]-datetime.date.today()).days))
+		 % (ret[1], ret[0].year, ret[0].month, ret[0].day, (ret[0]-datetime.date.today()).days))
 
