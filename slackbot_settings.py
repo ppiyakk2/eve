c6806ef6f2e8f67c24d68a88325e00995e7fcff0
yangroro <529yhs@gmail.com>
2017-03-15 16:46:36 +0900

    Added next pyjog event plugin

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,9 +1,15 @@
+import os
+
 #API_TOKEN = ""
+#FACEBOOK_APP_TOKEN = ''
 
 DEFAULT_REPLY = "봇 테스트는 #bot_test 에서 해주세요"
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 PLUGINS = [
     'pyjogbot.plugins.help',
     'pyjogbot.plugins.ebook_notify',
     'pyjogbot.plugins.rawbeef',
+    'pyjogbot.plugins.pyjog_event'
 ]
